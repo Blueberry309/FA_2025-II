a6e256e4caf03a35ee3ebdd9b3ba5261a5851a33
Ariana <arianasanchez309@gmail.com>
2025-09-02 15:31:07 -0500

    Subiendo el ejercicio 1

---
+++ Semana1_python/Semana1_python.py
@@ -0,0 +1,8 @@
+def ejer1():
+    nombre=input ("ingrese su nombre:")
+    carrera=input ("Ingrese su carrera:")
+    #esto es un comentario genial
+    print(f"{nombre}, bienvenido a Fundamentos de algoritmos de {carrera}")
+
+
+ejer1()
